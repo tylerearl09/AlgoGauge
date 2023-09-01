468faeb6edf3bcca73198fd5fe30101aaf11f557
tylerearl09 <107437480+tylerearl09@users.noreply.github.com>
2023-09-01 17:43:48 -0600

    added connection to MongoDB and form to submit data to it

--- algo-client/src/App.js
+++ algo-client/src/App.js
@@ -1,7 +1,28 @@
 import logo from './icon-256x256.png';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("")
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    let result = await fetch(
+      'http://localhost:5000/register', {
+        method: "post",
+        body: JSON.stringify({name, email}),
+        headers: {
+          'Content-Type' : 'application/json'
+        }
+      })
+    result = await result.json();
+    console.warn(result);
+    if (result){
+      alert("Data saved succesfully");
+      setEmail("");
+      setName("");
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,9 +30,18 @@ function App() {
         <p>
           Hello, Brad.
         </p>
-        
+        <form action="">
+        <input type="text" placeholder="name"
+        value={name} onChange={(e) => setName(e.target.value)} />
+        <input type="email" placeholder="email"
+        value={email} onChange={(e) => setEmail(e.target.value)} />
+        <button type="submit"
+        onClick={handleOnSubmit}>submit</button>
+      </form>       
       </header>
+      
     </div>
+   
   );
 }
 

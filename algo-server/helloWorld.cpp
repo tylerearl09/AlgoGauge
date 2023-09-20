24889c8eb11ca12d9ca0e4fd369b33d18118280f
Caden Schindler <greatwarman@gmail.com>
2023-09-19 19:47:38 -0600

    Added ArgumentParsing

--- algo-server/helloWorld.cpp
+++ algo-server/helloWorld.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
+#include <argparse/argparse.hpp>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
-string first = argv[1];
+  argparse::ArgumentParser program("program_name");
 
-cout << first << " World!" << endl;
+  program.add_argument("--firstWord");
+  program.add_argument("--secWord");
 
- return 0;
+  try {
+    program.parse_args(argc, argv);
+  }
+  catch (const std::runtime_error& err) {
+    std::cerr << err.what() << std::endl;
+    std::cerr << program;
+    std::exit(1);
+  }
+
+  cout << program.get("--firstWord") << program.get("--secWord") << endl;
+
+  return 0;
 }

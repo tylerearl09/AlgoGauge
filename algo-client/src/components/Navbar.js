ee526ed7b6acee34bda1ed8777806a3fa41ef30c
Caden Schindler <greatwarman@gmail.com>
2023-10-03 19:54:08 -0600

    Fixed Hamburger Dropdown

--- algo-client/src/components/Navbar.js
+++ algo-client/src/components/Navbar.js
@@ -3,7 +3,7 @@ function NavBar () {
     return(
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark ps-5">
         <a class="navbar-brand" href="#">Aolguage</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">

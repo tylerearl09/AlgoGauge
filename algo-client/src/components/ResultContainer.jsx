33c89945450384a505a32a26daeb2ecd45f1367e
Karl Albornoz <albornoz.karl13@gmail.com>
2023-11-30 20:21:44 -0700

    added styling to results page

--- algo-client/src/components/ResultContainer.jsx
+++ algo-client/src/components/ResultContainer.jsx
@@ -1,25 +1,48 @@
 
 const winnerStyle = 
 {
-    color: 'green'
+    color: 'yellow'
 };
 
 const loserStyle =
 {
-    color: 'yellow'
+    color: 'white'
 };
 export default function ResultContainer(props){
 
     return(
-        <div className="col-lg rounded-4 bg-dark m-3 p-3" style={props.winner ? winnerStyle : loserStyle}>
+        <div className={`col-lg rounded-4 bg-dark m-3 p-3 `+ (props.winner ? "text-warning-emphasis" : "text-white")}>
             <h3 className="font-monospace text-center">
                 Algorithm # {props.id}
             </h3>
             <hr className="mb-4" />
-            <p>Algorithm:  {props.algoName}</p>
-            <p>Amount: {props.amount}</p>
-            <p>Option: {props.mod}</p>
-            <p>Run Time: {props.runTime} ms</p>
+            <div className="ms-3">
+                <div className="row just">
+                    <div className="col-md-8 d-flex justify-content-between fs-4 fw-bolder">
+                        <p>Sorting Algorithm:</p>
+                        <p>{props.algoName}</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-8 d-flex justify-content-between fs-4 fw-bolder">
+                        <p>Number of Items:</p>
+                        <p>{props.amount}</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-8 d-flex justify-content-between fs-4 fw-bolder">
+                        <p>Data Distribution:</p>
+                        <p>{props.mod}</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-8 d-flex justify-content-between fs-4 fw-bolder">
+                        <p>Run Time:</p>
+                        <p>{props.runTime}</p>
+                    </div>
+                </div>
+            </div>
+            
             
         </div>
     )

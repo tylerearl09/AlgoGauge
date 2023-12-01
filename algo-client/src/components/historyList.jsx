7b137cd46e643d0065fd0f336f47acca6d4e1357
tylerearl09 <107437480+tylerearl09@users.noreply.github.com>
2023-11-30 19:34:46 -0700

    fixed side scroll on results page and historylist component

--- algo-client/src/components/historyList.jsx
+++ algo-client/src/components/historyList.jsx
@@ -58,7 +58,7 @@ export default function HistoryList() {
 
   return (
     <div>
-      <div className="row justify-content-center">
+      <div className="row m-auto justify-content-center">
         <div className="col-md-8">
           <h2 className="text-center border-bottom display-5 mt-3">
             PAST TESTS
